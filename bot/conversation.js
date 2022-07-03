9582fc64730334db3adc252500c3dda90a7da9bd
Emma <30394334+shamisem@users.noreply.github.com>
2022-07-03 15:27:34 +0300

    Update conversation.js

--- bot/conversation.js
+++ bot/conversation.js
@@ -12,7 +12,7 @@ const chat = {
         text: 'I specialize in cyber security, blockchain, and data analytics. I have experience with both developer and end-user documentation, and I love the Docs as Code approach.',
         options: [
             {
-                text: "That's nice, could I see your CV and portfolio?",
+                text: "That's nice, can I see your CV and portfolio?",
                 next: 3
             },
             {
@@ -25,7 +25,7 @@ const chat = {
         text: 'Of course. I have a portfolio website built with GitBook: <a target="_blank" href="https://emmaos.gitbook.io/portfolio/">Portfolio</a>. <br> My CV is available in PDF format (<a href="https://www.emmatechwriter.com/EArakelyan_TW.pdf">View <i aria-hidden="true" class="fas fa-arrow-right"></i></a>). You can also browse my work history on LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/emmatechwriter">Emma Arakelyan</a>.',
         options: [
             {
-                text: 'I like these. How do I contact you?',
+                text: 'I like these. How can I contact you?',
                 next: 4
             }
         ]
